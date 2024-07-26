f1511fb2a9e04b87e3011fa5c14b8591c5902602
sapna sharma <sapnasharma12544.com>
2024-07-26 01:25:01 -0700

    update

--- program2.js
+++ program2.js
@@ -1,7 +1,35 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+  let sIndex = 0;
+  let pIndex = 0;
 
-  };
-  
-  module.exports = decodeTheRing;
+  while (sIndex < s.length && pIndex < p.length) {
+    if (p[pIndex] === '*') {
+
+      if (pIndex === p.length - 1) return true;
+
+      //kuldee s
+      //kuld*p  p
+      let nextChar = p[pIndex + 1];
+      let nextCharIndex = s.indexOf(nextChar, sIndex);
+
+      if (nextCharIndex === -1) return false;
+
+      // kuldeeeep
+      // kuld*p
+
+      sIndex = nextCharIndex;
+      pIndex++;
+    } else if (p[pIndex] === '?' || p[pIndex] === s[sIndex]) {
+      sIndex++;
+      pIndex++;
+    } else {
+      return false;
+    }
+  }
+
+  return sIndex === s.length && pIndex === p.length;
+
+};
+
+module.exports = decodeTheRing;
